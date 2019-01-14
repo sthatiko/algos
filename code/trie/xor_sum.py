d51081b4f32c05d34f488381eb5c1dbb7dc964e4
Shravan Thatikonda <shravan.thatikonda@oracle.com>
2019-01-14 16:59:59 +0530

    made some optimizations, yet TLE.

--- code/trie/xor_sum.py
+++ code/trie/xor_sum.py
@@ -1,36 +1,34 @@
 import os
 import sys
 
-int_size = 32
-
-
-class TrieNode:
-    def __init__(self, value=0, children=None):
-        self.value = value
-        if children is None:
-            self.children = [None]*2
-
 
 class Trie:
+    int_size = 32
+
+    class TrieNode:
+        def __init__(self, value=0):
+            self.value = value
+            self.children = [None, None]
+
     def __init__(self):
-        self.root = TrieNode()
+        self.root = Trie.TrieNode()
 
     def insert(self,  element):
         current = self.root
-        for cur_pos in range(int_size-1, 0, -1):
+        for cur_pos in range(Trie.int_size-1, 0, -1):
             cur_bit = 1 if (element & 1 << cur_pos) else 0
             if current.children[cur_bit] is None:
-                current.children[cur_bit] = TrieNode()
+                current.children[cur_bit] = Trie.TrieNode()
             current = current.children[cur_bit]
         current.value = element
 
     def query(self, pre_xor):
         current = self.root
-        for cur_pos in range(int_size-1, 0, -1):
+        for cur_pos in range(Trie.int_size-1, 0, -1):
             cur_bit = 1 if (pre_xor & 1 << cur_pos) else 0
             if current.children[1-cur_bit] is not None:
                 current = current.children[1-cur_bit]
-            elif current.children[cur_bit] is not None:
+            else:
                 current = current.children[cur_bit]
         return pre_xor ^ current.value
 
@@ -44,18 +42,23 @@ def problem_solver(int_arr):
         pre_xor = pre_xor ^ element
         trie.insert(pre_xor)
         result = max(result, trie.query(pre_xor))
-    print(result)
+    return result
 
 
 sys.stdin = open(os.path.abspath(__file__)[:-2] + 'in', 'r')  # TODO: for local testing only
-
-n_tc = int(input())
+data = sys.stdin.readlines()
+os.read()
+input_data = iter(data)
+n_tc = int(next(input_data))
+op = list()
 for tc in range(n_tc):
-    arr_length = int(input())
+    arr_length = int(next(input_data))
     arr = list()
     for i in range(arr_length):
-        arr.append(int(input()))
-    problem_solver(arr)
+        arr.append(int(next(input_data)))
+    op.append(problem_solver(arr))
+for l in op:
+    print(l)
 
 
 '''
@@ -69,7 +72,7 @@ answer is obtained by applying XOR logic on the corresponding bits of the set of
 7, 8 and 5 are XORed as follows,
 Numbers in binary: 0111 1000 0101 ----- 1010
 So the answer is 10 (in decimal). The same answer can be obtained in C/C++/Java by using the
-XOR operator as 7ˆ8ˆ5.
+XOR operator as 7 8 5.
 Input
 The first line contains the number of test cases T. The first line of each test-case contains one integer,
 N (size of the array). The next N lines of each test-case contain integers denoting the elements of the
@@ -77,9 +80,9 @@ array.
 Output
 For each test case, output a single line containing the maximum sum that can be obtained.
 Constraints:
-• 1 ≤ T ≤ 10
-• 1 ≤ N ≤ 100, 000
-• All input integers will be non-negative and fit into 32 bit signed integer.
+ 1  T  10
+ 1  N  100, 000
+ All input integers will be non-negative and fit into 32 bit signed integer.
 Sample Input
 2 5 3 7 7 7 0 5 3 8 2 6 4
 Sample Output
